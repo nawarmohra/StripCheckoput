d154524d93c55eb95e3aa6990311da0bff2f970a
Bara Abdullatif <bara.abd@yahoo.com>
2020-09-16 20:12:23 +0200

    Update jason

--- StripCheckoput/frontend/src/components/stripe-button.jsx
+++ StripCheckoput/frontend/src/components/stripe-button.jsx
@@ -4,8 +4,13 @@ import axios from 'axios';
 
 
 const StripeCheckoutButton = ({ price }) => {
+<<<<<<< Updated upstream
   const priceForStripe = price
   const publishableKey = 'pk_test_4TEQdfVlOd2xxbcZbKioEJP000GOEjENkH';
+=======
+  const priceForStripe = price 
+  const publishableKey = 'pk_test_YTiOUdn5AfnIYTb7qbY94XKV00rjG4dGyO';
+>>>>>>> Stashed changes
 
   const onToken = token => {
     console.log("pay")
@@ -33,11 +38,19 @@ const StripeCheckoutButton = ({ price }) => {
   return (
     <StripeCheckout
       label='Pay Now'
+<<<<<<< Updated upstream
       name='TechStore Mobile Store.'
       billingAddress
       shippingAddress
       image='https://svgshare.com/i/CUz.svg'
       description={`Your total is kr${price}`}
+=======
+      name='Mobil Website'
+      billingAddress
+      shippingAddress
+      image=''
+      description={`Your total is $${price}`}
+>>>>>>> Stashed changes
       amount={priceForStripe}
       panelLabel='Pay Now'
       token={onToken}
